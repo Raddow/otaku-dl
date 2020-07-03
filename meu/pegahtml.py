7c00debdc8d68530046c086047809b3e2bd35ba9
Raddow <rmtrakizera4@gmail.com>
2020-07-02 21:51:37 -0300

    delete dir meu/

--- meu/pegahtml.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_html(url):
-    """
-    Gets html text for the given url
-    """
-    response = requests.get(url)
-    return response.text  # Converts the response into text and return it
